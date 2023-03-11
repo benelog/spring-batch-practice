7cb6dcf81e2b58b3fa648a1fc75a75ed62c8fa0d
Sanghyuk Jung <sanghyuk.jung@navercorp.com>
2023-03-11 11:09:06 +0900

    2장 예제 추가

---
+++ ch02-log-batch/src/main/java/kr/co/wikibook/batch/logbatch/CountAccessLogTask.java
@@ -0,0 +1,26 @@
+package kr.co.wikibook.batch.logbatch;
+
+import javax.sql.DataSource;
+import org.slf4j.ILoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CountAccessLogTask implements CommandLineRunner {
+
+  private final JdbcTemplate jdbc;
+  private final Logger logger;
+
+  public CountAccessLogTask(DataSource dataSource, ILoggerFactory loggerFactory) { // <2>
+    this.jdbc = new JdbcTemplate(dataSource); // <3>
+    this.logger = loggerFactory.getLogger(CountAccessLogTask.class.getName());
+  }
+
+  @Override
+  public void run(String... args) {
+    long count = jdbc.queryForObject("SELECT COUNT(1) FROM access_log", Long.class);
+    logger.info("access_log 테이블의 건 수 : {}", count); // <4>
+  }
+}
