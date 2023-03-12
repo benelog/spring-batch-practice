4b4e18c25f222231d082042cf828c82a56d8c942
Sanghyuk Jung <sanghyuk.jung@navercorp.com>
2023-03-12 21:08:57 +0900

    3장 예제 추가

---
+++ ch03-log-batch/src/test/java/kr/co/wikibook/batch/logbatch/UserAccessSummaryCsvWriterTest.java
@@ -0,0 +1,33 @@
+package kr.co.wikibook.batch.logbatch;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+import org.springframework.core.io.FileSystemResource;
+
+class UserAccessSummaryCsvWriterTest {
+  @Test
+  void write(@TempDir Path tempPath) throws Exception {
+    // given
+    String outputPath = tempPath.toString() + "/user-access-summary.csv";
+    var resource = new FileSystemResource(outputPath);
+    var writer = new UserAccessSummaryCsvWriter(resource);
+    var items = List.of(
+        new UserAccessSummary("benelog", 32),
+        new UserAccessSummary("jojoldu", 42)
+    );
+
+    // when
+    writer.open();
+    writer.write(items);
+    writer.close();
+
+    // then
+    List<String> written = Files.readAllLines(Path.of(outputPath));
+    assertThat(written).isEqualTo(List.of("benelog,32", "jojoldu,42"));
+  }
+}
