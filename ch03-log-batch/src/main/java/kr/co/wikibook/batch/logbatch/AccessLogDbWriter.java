15b1219d2b6de1f5e4b58f6fb8fb6125c92e79e7
Sanghyuk Jung <sanghyuk.jung@navercorp.com>
2025-06-20 06:31:43 +0900

    13장 예제, 3장 예제

--- ch03-log-batch/src/main/java/kr/co/wikibook/batch/logbatch/AccessLogDbWriter.java
+++ ch03-log-batch/src/main/java/kr/co/wikibook/batch/logbatch/AccessLogDbWriter.java
@@ -3,11 +3,12 @@ package kr.co.wikibook.batch.logbatch;
 import java.util.List;
 import javax.sql.DataSource;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 public class AccessLogDbWriter {
 
-  private final NamedParameterJdbcTemplate jdbc;
+  private final NamedParameterJdbcOperations jdbc;
 
   public AccessLogDbWriter(DataSource dataSource) {
     this.jdbc = new NamedParameterJdbcTemplate(dataSource);
